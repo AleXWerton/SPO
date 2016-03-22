a3d811544370000f610f0e9d8f0cebf4d3cb9e30
AleXWerton <chopalavov221@gmail.com>
2016-03-22 22:10:39 +0300

    Update Reg_Expr.java

--- src/Reg_Expr.java
+++ src/Reg_Expr.java
@@ -14,7 +14,7 @@ public class Reg_Expr
         String[] a = new String[4];
         Scanner b = new Scanner(new File ("1.txt"));
         int i = 0, x = 0, y = 0, z = 0;
-        System.out.println("Строки:\n");
+        System.out.println("Lines:\n");
         while(b.hasNext())
         {
         a[i] = b.nextLine();
@@ -22,11 +22,11 @@ public class Reg_Expr
         z = match.match(a[x]);
         y += z;
         x++;
-        System.out.println("Совпадения в " +x + " строке: " + z + "\n");
+        System.out.println("Matches in " +x + " line: " + z + "\n");
         z = 0;
         i++;
         }
-        System.out.println("\nВсего совпадений в тексте: " + y);   
+        System.out.println("\nMatches in whole text: " + y);   
         b.close();
     }
     
